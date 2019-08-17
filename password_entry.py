38d9e770d499b55cab9875beade2e39d01418730
jc314416 <30332529+jc314416@users.noreply.github.com>
2019-08-17 12:58:36 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,12 @@
 """Russell"""
+MINIMUM_PASSWORD_LENGTH = 7
+valid_password = False
+while not valid_password:
+    password = input("Enter password: ")
+    if len(password) < MINIMUM_PASSWORD_LENGTH:
+        print("Password is too short")
+    else:
+        valid_password = True
+
+for i in range(len(password)):
+    print("*", end="")
